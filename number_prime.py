45618d04198ccfe24daef7536f8c47a468e572d0
difo23 <lizandro23difo@gmail.com>
2022-07-26 22:12:02 -0400

    Count numbers

--- number_prime.py
+++ number_prime.py
@@ -3,24 +3,29 @@
 # number_pairs - snake_case - python
 
 
+def is_prime(n):
+    for div in range(2, n // 2 + 1):
+        if n % div == 0:
+            return False
+    return True
 
 
  # not and or
-def number_even(nums):
-    count_even = 0
+def number_prime(nums):
+    count_prime = 0
     
     for num in nums:
-        if num % 2 = 0:
-            count_even += 1
+        if not is_prime(num):
+            count_prime += 1
             
-    return count_even
+    return count_prime
 
 # Example:
 nums_list = [3, 5, 3, 6, 2, 7, 8, 6, 9]
 
 # Result: 4
 
-result = number_even(nums_list)
+result = number_prime(nums_list)
 
 if result ==  5:
     print("Correct:", result)
